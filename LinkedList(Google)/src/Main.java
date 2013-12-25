b9d39ce4059babeb342b68bcc295dfe5ae6f057b
5heri <sher.khan12@imperial.ac.uk>
2013-12-25 06:50:25 +0500

    ll

--- LinkedList(Google)/src/Main.java
+++ LinkedList(Google)/src/Main.java
@@ -1,5 +1,6 @@
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 
 public class Main {
@@ -38,14 +39,14 @@ public class Main {
 		Node x = findKtoLast(n1, 2);
 		System.out.println(x.getValue());
 		
-		//deleteCurrent(n5);
+		/* deleteCurrent(n5);
 		
 		root = n1;
 		while (root != null) {
 			System.out.print(root.getValue() + " ");
 			root = root.getNext();
 		}
-		System.out.println();
+		System.out.println(); */
 		
 		root = partitionAround(n1, 5);
 		while (root != null) {
@@ -54,8 +55,114 @@ public class Main {
 		}
 		System.out.println();
 		
+		Node test1 = new Node(7);
+		Node test2 = new Node(1);
+		Node test3 = new Node(6);
+		test1.setNext(test2);
+		test2.setNext(test3);
 		
+		
+		Node test4 = new Node(5);
+		Node test5 = new Node(9);
+		Node test6 = new Node(2);
+		test4.setNext(test5);
+		test5.setNext(test6);
+		
+		Node added = addLists(test1, test4);
+		while (added != null) {
+			System.out.print(added.getValue() + " ");
+			added = added.getNext();
+		}
+		System.out.println();
+		
+		
+		Node node1 = new Node(1);
+		Node node2 = new Node(2);
+		Node node3 = new Node(3);
+		Node node4 = new Node(3);
+		Node node5 = new Node(2);
+		Node node6 = new Node(1);
+		Node midNode = new Node(10);
+		node1.setNext(node2);
+		node2.setNext(node3);
+		node3.setNext(midNode);
+		midNode.setNext(node4);
+		node4.setNext(node5);
+		node5.setNext(node6);
+		
+		boolean isPalin = isPalindrome(node1);
+		
+		while (node1 != null) {
+			System.out.print(node1.getValue() + " ");
+			node1 = node1.getNext();
+		}
+		System.out.println();
+		System.out.println(isPalin);
+		
+		
+	}
+	
+	private static boolean isPalindrome(Node node) {
+		if (node == null) {
+			return true;
+		}
+		Node fast = node;
+		Node slow = node;
 
+		Stack<Node> stack = new Stack<Node>();
+		
+		while (fast != null && fast.getNext() != null) {
+			stack.push(slow);
+			slow = slow.getNext();
+			fast = fast.getNext().getNext();
+		}
+		
+		// In even fast = null
+		// In odd fast.next = null
+		if (fast != null) {
+			slow = slow.getNext();
+		}
+		while (slow != null) {
+			if (!slow.equals(stack.pop())) {
+				return false;
+			}
+			slow = slow.getNext();
+		}
+		return true;
+	}
+	
+	// All lists are in reverse order of the answers.
+	private static Node addLists(Node node1, Node node2) {
+		StringBuffer sb1 = new StringBuffer();
+		StringBuffer sb2 = new StringBuffer();
+		
+		while (node1 != null) {
+			sb1.append(node1.getValue());
+			node1 = node1.getNext();
+		}
+		
+		while (node2 != null) {
+			sb2.append(node2.getValue());
+			node2 = node2.getNext();
+		}
+		sb1.reverse();
+		sb2.reverse();
+		
+		int sum = Integer.parseInt(sb1.toString()) + Integer.parseInt(sb2.toString());
+		
+		sb1 = new StringBuffer();
+		sb1.append(sum);
+		Node head = null;
+		for (int i = 0; i < sb1.length(); i++) {
+			Node newNode = new Node(Character.getNumericValue(sb1.charAt(i)));
+			if (head == null) {
+				head = newNode;
+			} else {
+				newNode.setNext(head);
+				head = newNode;
+			}
+		}
+		return head;
 	}
 	
 	//REMINDER: the first element in the 2nd list is not the pivot
